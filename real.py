0e04226e715f04b9c2e75b196e482ee2d63ea21d
anand498 <anand.pandey803@gmail.com>
2019-08-10 19:02:52 +0530

    adding scripts for dataset creation

---
+++ real.py
@@ -0,0 +1,40 @@
+import cv2
+import sys
+import random
+import os
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+    cv2.rectangle(frame, (400, 100), (900, 550), (255,0,0), 2)
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        var1=frame.copy()
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print(x,y,x+w,y+h)
+        if(x<800 and x>400 and y<300 and y>100 and (x+w)<900 and (x+w)>400 and (y+h)<560 and (y+h) and len(faces)==1):
+            cv2.putText(frame,"Perfect", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.imwrite(os.path.join('/Users/anand498/Desktop/capture/real',"real%d.jpg" % random.randint(40000,90000)), var1) 
+
+  
+    label = "{}".format(len(faces))
+    #cv2.putText(frame,label, (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
