5b136409c9ede8b802c95f624ac0a1f7da79d26b
anand498 <anand.pandey803@gmail.com>
2019-08-21 08:33:29 +0530

    modifications in real image capture script

--- real.py
+++ real.py
@@ -17,6 +17,7 @@ while True:
         minNeighbors=5,
         minSize=(30, 30),
     )
+    
     cv2.rectangle(frame, (400, 100), (900, 550), (255,0,0), 2)
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
@@ -24,6 +25,8 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         print(x,y,x+w,y+h)
         if(x<800 and x>400 and y<300 and y>100 and (x+w)<900 and (x+w)>400 and (y+h)<560 and (y+h) and len(faces)==1):
+            #if the face coems inside and teh dimensionsof the video is more than half the size of window
+            # the frame will be captured.
             cv2.putText(frame,"Perfect", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             cv2.imwrite(os.path.join('/Users/anand498/Desktop/capture/real',"real%d.jpg" % random.randint(40000,90000)), var1) 
 
@@ -31,9 +34,9 @@ while True:
     label = "{}".format(len(faces))
     #cv2.putText(frame,label, (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv2.imshow('Video', frame)
-
+    # to show frames consecutively
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        break#on pressing the 'q' button the frame capturing will end.
 
 # When everything is done, release the capture
 video_capture.release()
